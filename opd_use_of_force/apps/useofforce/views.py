5c75557e17992705852cdd2c78eb1a2edd0e56c4
Aaron Williams <aaron.colby.williams@gmail.com>
2015-08-27 15:36:53 -0700

    scope date to Oakland and 2005 to present

--- opd_use_of_force/apps/useofforce/views.py
+++ opd_use_of_force/apps/useofforce/views.py
@@ -1,9 +1,15 @@
-from bakery.views import BuildableListView, BuildableDetailView
-
+from bakery.views import BuildableTemplateView
+from django.db.models import Q
 from opd_use_of_force.apps.useofforce.models import Incident
 
 
-class IncidentListView(BuildableListView):
-    model = Incident
-    context_object_name = 'incidents'
-    template_name = 'incident_map.html' 
+class IncidentListView(BuildableTemplateView):
+    template_name = 'incident_map.html'
+
+    def get_context_data(self, **kwargs ):
+        context = super(IncidentListView, self).get_context_data(**kwargs)
+        context['incidents'] = Incident.objects.filter(
+            Q(city='Oakland'),
+            Q(date__range=['2006-01-01', '2015-12-31'])
+        )
+        return context
